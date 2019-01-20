ea63803cded65fa073b3a6cd5798233d301047d6
Mark Ellis <memellis@gmail.com>
2019-01-20 15:48:23 +0000

    Removed redundant code

--- src/java/com/ellzone/slotpuzzle2d/prototypes/minislotmachine/SpinningSlotsWithMatchesWinFlashesLoadedLevel.java
+++ src/java/com/ellzone/slotpuzzle2d/prototypes/minislotmachine/SpinningSlotsWithMatchesWinFlashesLoadedLevel.java
@@ -188,107 +188,6 @@ public class SpinningSlotsWithMatchesWinFlashesLoadedLevel extends SPPrototypeTe
         return level.getLayers().get(SLOT_REEL_OBJECT_LAYER).getObjects().getByType(RectangleMapObject.class);
     }
 
-    private void createLevel(Array<RectangleMapObject> levelRectangleMapObjects) throws IllegalAccessException {
-        for (RectangleMapObject rectangleMapObject : levelRectangleMapObjects) {
-            MapProperties rectangleMapObjectProperties = rectangleMapObject.getProperties();
-            String className = (String) rectangleMapObjectProperties.get("Class");
-            System.out.println(String.format("creating className: %s", className));
-            Class<?> clazz = null;
-            try {
-                 clazz = Class.forName(className);
-            } catch (ClassNotFoundException cnfe) {
-                System.out.println(String.format("Class not found %s.", clazz));
-            }
-            Array<String> constructorParameters = getClassConstructorParameters(rectangleMapObjectProperties, "Parameter");
-            Class<?>[] classParameters = getParamTypes(constructorParameters);
-
-            Constructor<?> constructor = null;
-            try {
-                constructor = clazz.getConstructor(classParameters);
-            } catch (NoSuchMethodException nsme) {
-                System.out.println(String.format("No such constructor exception %s",
-                                                  nsme.getMessage()));
-
-            }
-            Array<String> constructorParameterValues = getClassConstructorParameters(rectangleMapObjectProperties, "ParameterValue");
-            Object[] constructorParanetersValues = null;
-            myLightButton = null;
-            try {
-                constructorParanetersValues =  parseConstructorParameterValues(constructorParameterValues, classParameters, rectangleMapObjectProperties);
-                myLightButton =  (HoldLightButton) constructor.newInstance(constructorParanetersValues);
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-            }
-
-            if (myLightButton != null) {
-                Field field = null;
-                try {
-                    field = this.getClass().getDeclaredField("lightButtons");
-                } catch (NoSuchFieldException e) {
-                    e.printStackTrace();
-                }
-                field.setAccessible(true);
-                myLightButton.addTo(field.get(this));
-            }
-        }
-    }
-
-    private Object[] parseConstructorParameterValues(Array<String> constructorParameterValues, Class<?>[] classParameters, MapProperties rectangleMapProperties) throws NoSuchFieldException, IllegalAccessException {
-        Object[] parameterValues = new Object[constructorParameterValues.size];
-        int index = 0;
-        for (String parameter : constructorParameterValues) {
-            parameterValues[index] = parseParameterValue(parameter, classParameters[index], rectangleMapProperties);
-            index++;
-        }
-        return parameterValues;
-    }
-
-    private Object parseParameterValue(String parameter, Class<?> classParam, MapProperties rectangleMapProperties) throws NoSuchFieldException, IllegalAccessException {
-        if (parameter.toLowerCase().startsWith("field"))
-            return parseField(parameter, classParam);
-        if (parameter.toLowerCase().startsWith("property"))
-            return parseProperty(parameter, classParam, rectangleMapProperties);
-        return null;
-    }
-
-    private Object parseField(String parameter, Class<?> classParam) throws NoSuchFieldException, IllegalAccessException {
-        String[] parts = parameter.split("\\.");
-        String fieldPart = parts.length == 2 ? parts[1] : null;
-        Field field = this.getClass().getDeclaredField(fieldPart);
-        return field.get(this);
-    }
-
-    private Object parseProperty(String parameter, Class<?> classParam, MapProperties rectangleMapProperties) {
-        String[] parts = parameter.split("\\.");
-        if (isInt(classParam)) {
-            int intParameter = Math.round(parts.length == 2 ? (Float) (Object) rectangleMapProperties.get(parts[1].toLowerCase(), classParam) : null);
-            return intParameter;
-        }
-        else
-            return parts.length == 2 ? (Object) rectangleMapProperties.get(parts[1].toLowerCase(), classParam) : null;
-    }
-
-    private boolean isFloat(Class clazz) {
-        return clazz.equals(float.class);
-    }
-
-    private boolean isInt(Class clazz) {
-        return clazz.equals(int.class);
-    }
-
-    private Array<String> getClassConstructorParameters(MapProperties rectangleMapObjectProperties, String key) {
-       int parameterCount = 1;
-       Array<String> parameters = new Array<>();
-       String parameterKey = key + String.valueOf(parameterCount);
-       String parameterValue =  (String) rectangleMapObjectProperties.get(parameterKey);
-       while (parameterValue != null) {
-           parameters.add(parameterValue);
-           parameterKey = key + String.valueOf(++parameterCount);
-           parameterValue =  (String) rectangleMapObjectProperties.get(parameterKey);
-       }
-       return parameters;
-    }
-
     Map<String,Class> builtInMap = new HashMap<String,Class>();{
         builtInMap.put("int", Integer.TYPE );
         builtInMap.put("long", Long.TYPE );
@@ -300,23 +199,6 @@ public class SpinningSlotsWithMatchesWinFlashesLoadedLevel extends SPPrototypeTe
         builtInMap.put("void", Void.TYPE );
         builtInMap.put("short", Short.TYPE );
     }
-
-    public Class<?>[] getParamTypes(Array<String> parameters){
-        Class<?>[] classParameters = new Class<?>[parameters.size];
-        int index = 0;
-        for( String type : parameters) {
-            try {
-                if (builtInMap.containsKey(type))
-                    classParameters[index++] = builtInMap.get(type);
-                else
-                    classParameters[index++] = Class.forName(type);
-            } catch (ClassNotFoundException cnfe) {
-                System.out.println(cnfe.getMessage());
-            }
-        }
-        return classParameters;
-    }
-
 
     @Override
     public void resize(int width, int height) {
