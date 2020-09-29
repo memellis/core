8dfe5186356afb4f6b11d64dc2117661c2309ce1
Mark Ellis <memellis@gmail.com>
2020-09-29 23:38:04 +0100

    Clean up - removed code into SpinWheelForSlotPuzzle with working debugBox2dRenderer

--- src/java/com/ellzone/slotpuzzle2d/prototypes/spin/SpinWheelWithoutStage.java
+++ src/java/com/ellzone/slotpuzzle2d/prototypes/spin/SpinWheelWithoutStage.java
@@ -18,53 +18,44 @@ package com.ellzone.slotpuzzle2d.prototypes.spin;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.IntArray;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.ellzone.slotpuzzle2d.prototypes.SPPrototype;
 import com.ellzone.slotpuzzle2d.spin.SpinWheel;
 import com.ellzone.slotpuzzle2d.spin.SpinWheelForSlotPuzzle;
 
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.scaleTo;
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
-
 public class SpinWheelWithoutStage extends SPPrototype {
-    private static final float WHEEL_DIAMETER = 750F;
+    private static final float WHEEL_DIAMETER = 500F;
     private static final int NUMBER_OF_PEGS = 12;
+    private static final float GRAVITY = -9.80f;
 
     private static final String TAG = SpinWheelWithoutStage.class.getSimpleName();
     private World world;
     private SpinWheelForSlotPuzzle spinWheel;
-    private Stage stage;
     private SpriteBatch batch;
     private Image wheelImage;
     private Image needleImage;
     private boolean box2dDebugRender = true;
     private Box2DDebugRenderer renderer;
+    private OrthographicCamera camera;
 
     @Override
     public void create() {
-        stage = new Stage(new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
         batch = new SpriteBatch();
-        Gdx.input.setInputProcessor(stage);
-
-        world = new World(new Vector2(0, -10), true);
+        world = new World(new Vector2(0, GRAVITY), true);
         renderer = new Box2DDebugRenderer();
-
+        camera = new OrthographicCamera(
+                Gdx.graphics.getWidth() / SpinWheel.PPM,
+                Gdx.graphics.getHeight() / SpinWheel.PPM);
+        camera.setToOrtho(false,
+                Gdx.graphics.getWidth() / SpinWheel.PPM,
+                Gdx.graphics.getHeight() / SpinWheel.PPM);
         setUpSpinWheel();
     }
 
@@ -75,69 +66,7 @@ public class SpinWheelWithoutStage extends SPPrototype {
                 Gdx.graphics.getHeight() / 2,
                 NUMBER_OF_PEGS,
                 world);
-
-        final TextureAtlas atlas = new TextureAtlas("spin/spin_wheel_ui.atlas");
-
-        setUpSpinWheelBody(atlas);
-
-        setUpSpinWheelSpinButton(atlas);
-
-        setUpSpinWheelNeedleBody(atlas);
-
-        setElementData();
-    }
-
-    private void setUpSpinWheelNeedleBody(TextureAtlas atlas) {
-        spinWheel.getNeedleBody().setUserData(needleImage = new Image(new Sprite(atlas.findRegion("needle"))));
-        updateCoordinates(spinWheel.getNeedleBody(), needleImage, 0, -25F);
-        needleImage.setOrigin(spinWheel.getNeedleCenterX(needleImage.getWidth()), spinWheel.getNeedleCenterY(needleImage.getHeight()));
-    }
-
-    private void setUpSpinWheelSpinButton(TextureAtlas atlas) {
-        final Image btnSpin = new Image(atlas.findRegion("spin_button"));
-        btnSpin.setOrigin(Align.center);
-        btnSpin.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, Align.center);
-
-        btnSpin.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                btnSpin.addAction(sequence(scaleTo(1.25F, 1.25F, 0.10F), scaleTo(1F, 1F, 0.10F)));
-                spinWheel.spin(MathUtils.random(5F, 30F));
-
-                Gdx.app.debug(TAG, "Spinning.");
-            }
-        });
-    }
-
-    private void setUpSpinWheelBody(TextureAtlas atlas) {
-        spinWheel.getWheelBody().setUserData(wheelImage = new Image(atlas.findRegion("spin_wheel")));
-        updateCoordinates(spinWheel.getWheelBody(), wheelImage, 0, 0);
-        wheelImage.setOrigin(Align.center);
-    }
-
-    private void updateCoordinates(Body body, Image image, float incX, float incY) {
-        image.setPosition((body.getPosition().x * SpinWheel.PPM) + incX, (body.getPosition().y * SpinWheel.PPM) + incY, Align.center);
-        image.setRotation(body.getAngle() * MathUtils.radiansToDegrees);
-    }
-
-    private void setElementData() {
-        spinWheel.addElementData(Color.valueOf("e966ac"), getData(1, 2));
-        spinWheel.addElementData(Color.valueOf("b868ad"), getData(2, 3));
-        spinWheel.addElementData(Color.valueOf("8869ad"), getData(3, 4));
-        spinWheel.addElementData(Color.valueOf("3276b5"), getData(4, 5));
-        spinWheel.addElementData(Color.valueOf("33a7d8"), getData(5, 6));
-        spinWheel.addElementData(Color.valueOf("33b8a5"), getData(6, 7));
-        spinWheel.addElementData(Color.valueOf("a3fd39"), getData(7, 8));
-        spinWheel.addElementData(Color.valueOf("fff533"), getData(8, 9));
-        spinWheel.addElementData(Color.valueOf("fece3e"), getData(9, 10));
-        spinWheel.addElementData(Color.valueOf("f9a54b"), getData(10, 11));
-        spinWheel.addElementData(Color.valueOf("f04950"), getData(12, 1));
-    }
-
-    private IntArray getData(int peg_1, int peg_2) {
-        IntArray array = new IntArray(2);
-        array.addAll(peg_1, peg_2);
-        return array;
+        spinWheel.setUpSpinWheel();
     }
 
     public void render() {
@@ -146,12 +75,12 @@ public class SpinWheelWithoutStage extends SPPrototype {
         update(delta);
 
         if (!spinWheel.spinningStopped()) {
-            updateCoordinates(spinWheel.getWheelBody(), wheelImage, 0, 0);
-
-            updateCoordinates(spinWheel.getNeedleBody(), needleImage, 0, -25F);
-        } else {
+            spinWheel.updateCoordinates(
+                    spinWheel.getWheelBody(), spinWheel.getWheelImage(), 0, 0);
+            spinWheel.updateCoordinates(
+                    spinWheel.getNeedleBody(), spinWheel.getNeedleImage(), 0, -25F);
+        } else
             System.out.println("lucky element is: " + spinWheel.getLuckyWinElement());
-        }
 
         if (Gdx.input.isKeyPressed(Input.Keys.ENTER))
             spinWheel.spin(0.2F);
@@ -159,25 +88,31 @@ public class SpinWheelWithoutStage extends SPPrototype {
         if (Gdx.input.isKeyPressed(Input.Keys.S))
             spinWheel.spin(MathUtils.random(5F, 30F));
 
+        batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        wheelImage.draw(batch, 1.0f);
-        needleImage.draw(batch, 1.0f);
+        spinWheel.getWheelImage().draw(batch, 1.0f);
+        spinWheel.getNeedleImage().draw(batch, 1.0f);
         batch.end();
+        if (box2dDebugRender)
+            box2dDebugRender();
+    }
+
+    private void box2dDebugRender() {
+        renderer.render(world, camera.combined);
     }
 
     private void update(float delta) {
         world.step(1 / 60f, 8, 2);
-        if (box2dDebugRender)
-            renderer.render(world, stage.getCamera().combined);
     }
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height);
     }
 
     @Override
     public void dispose() {
-        stage.dispose();
+        batch.dispose();
+        world.dispose();
+        renderer.dispose();
     }
 }
